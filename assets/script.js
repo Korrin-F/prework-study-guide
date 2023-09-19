d07475295e68089dd225405948e1b38e29677c7d
Korrin-F <96596840+Korrin-F@users.noreply.github.com>
2023-09-19 15:18:55 +0100

    added topics array and for loop that prints topics to console js

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,21 @@
-var topics = "Terminal, HTML, CSS, Git, Javascript";
-console.log(topics);
+var topics = ['Terminal', 'HTML', 'CSS', 'Git', 'JavaScript'];
+for(var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+   }
+   
+/*
+if (topic === 'HTML') {
+  console.log("Let's study HTML!");
+} else if (topic === 'Terminal') {
+  console.log("Let's study Terminal!");
+} else if (topic === 'CSS') {
+  console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+  console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+  console.log("Let's study JavaScript!");
+} else {
+  console.log('Please try again!');
+}
+*/
+
